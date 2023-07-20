8faa612ead8d587cfc85b19189eed5f17fb23d89
yr623 <yeahram623@gmail.com>
2023-07-20 22:29:43 +0900

    empty component생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
 import Image from "next/image";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello, Airbnb</div>;
+  const isEmpty = true;
+  if (isEmpty) {
+    <ClientOnly>
+      <EmptyState showReset />
+    </ClientOnly>;
+  }
+  return (
+    <div>
+      <ClientOnly>
+        <Container>
+          <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+            <div>My future listings</div>
+          </div>
+        </Container>
+      </ClientOnly>
+    </div>
+  );
 }
