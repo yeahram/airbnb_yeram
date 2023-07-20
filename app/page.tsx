0b59db5700ee3b52b5d8104c152c1cb527e1fe57
yr623 <yeahram623@gmail.com>
2023-07-20 23:07:40 +0900

    listingCard컴포넌트 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import getListings from "./actions/getListings";
 import Image from "next/image";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import getCurrentUser from "./actions/getCurrentUser";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
   const isEmpty = true;
   if (isEmpty) {
     <ClientOnly>
@@ -15,7 +27,15 @@ export default function Home() {
       <ClientOnly>
         <Container>
           <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-            <div>My future listings</div>
+            {listings.map((listing: any) => {
+              return (
+                <ListingCard
+                  currentUser={currentUser}
+                  key={listing.id}
+                  data={listings}
+                />
+              );
+            })}
           </div>
         </Container>
       </ClientOnly>
