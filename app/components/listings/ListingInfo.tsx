12df6b93e225f13aa421496c4fbb341f23c58a7c
yr623 <124855516+yeahram@users.noreply.github.com>
2023-07-25 13:24:07 +0900

    listingInfo수정

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 import { IconType } from "react-icons";
+import Avatar from "../Avatar";
 
 interface ListingInfoProps {
   user: SafeUser;
@@ -19,7 +21,42 @@ interface ListingInfoProps {
   locationValue: string;
 }
 
-const ListingInfo = () => {
-  return <>listing Info!</>;
+const ListingInfo: React.FC<ListingInfoProps> = ({
+  user,
+  description,
+  guestCount,
+  roomCount,
+  bathroomCount,
+  category,
+  locationValue,
+}) => {
+  const { getByValue } = useCountries();
+  const coordinates = getByValue(locationValue)?.latlng;
+  return (
+    <div className="col-span-4 flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="text-xl font-semibold flex flex-row items-center gap-2">
+          <div>Hosted by {user.name}</div>
+          <Avatar src={user?.image} />
+        </div>
+        <div
+          className="
+          flex flex-row items-center gap-4 font-light text-neutral-500"
+        >
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} bathrooms</div>
+        </div>
+      </div>
+      <hr />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
+        ></ListingCategory>
+      )}
+    </div>
+  );
 };
 export default ListingInfo;
