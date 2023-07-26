f0d3da73382f1f06b5500890babb19f93cb89d64
yr623 <124855516+yeahram@users.noreply.github.com>
2023-07-26 14:26:14 +0900

    getListings 에러수정

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,16 +1,30 @@
 import prisma from "@/app/libs/prismadb";
 
-export default async function getListings() {
+export interface IListingsParams {
+  userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
+}
+export default async function getListings(params: IListingsParams) {
   try {
-    const listings = await prisma?.listing.findMany({
+    let query: any = {};
+    const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
-        createdAt: "desc",
+        createAt: "desc",
       },
     });
+
     const safeListings = listings.map((listing) => ({
       ...listing,
-      createdAt: listing.createdAt.toISOString(),
+      createdAt: listing.createAt.toISOString(),
     }));
+
     return safeListings;
   } catch (error: any) {
     throw new Error(error);
