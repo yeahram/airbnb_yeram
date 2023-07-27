a0e9fafa6a7f2f7369d19751ed48ea1874851c5f
yr623 <124855516+yeahram@users.noreply.github.com>
2023-07-27 18:34:47 +0900

    route생성

---
+++ app/api/reservations/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+  const body = await request.json();
+  const { listingId, startDate, endDate, totalPrice } = body;
+
+  if (!listingId || !startDate || !endDate || !totalPrice) {
+    return NextResponse.error();
+  }
+  cosntlistingAndReservation = await prisma.listing.update({});
+}
