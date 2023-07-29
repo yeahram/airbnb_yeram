8a1ba79e32791cdf397da50f56f3fb040ab201a8
yeahram <124855516+yeahram@users.noreply.github.com>
2023-07-29 15:27:32 +0900

    Create favoritesClient

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -2,6 +2,7 @@ import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
 import getFavoriteListings from "../actions/getFavoriteListing";
+import FavoritesClient from "./FavoritesClient";
 const ListingPage = async () => {
   const listings = await getFavoriteListings();
   const currentUser = await getCurrentUser();
@@ -24,3 +25,4 @@ const ListingPage = async () => {
     </ClientOnly>
   );
 };
+export default ListingPage;
