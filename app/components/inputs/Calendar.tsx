d3140de5d2ebacc2ff26d420d9fa1bce676ea4f9
yeahram <124855516+yeahram@users.noreply.github.com>
2023-08-05 14:04:35 +0900

    update calendar

--- app/components/inputs/Calendar.tsx
+++ app/components/inputs/Calendar.tsx
@@ -5,13 +5,13 @@ import { DateRange, Range, RangeKeyDict } from "react-date-range";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 
-interface CalendarProps {
+interface DatePickerProps {
   value: Range;
   onChange: (value: RangeKeyDict) => void;
   disabledDates?: Date[];
 }
 
-const Calendar: React.FC<CalendarProps> = ({
+const DatePicker: React.FC<DatePickerProps> = ({
   value,
   onChange,
   disabledDates,
@@ -21,13 +21,13 @@ const Calendar: React.FC<CalendarProps> = ({
       rangeColors={["#262626"]}
       ranges={[value]}
       date={new Date()}
+      onChange={onChange}
       direction="vertical"
       showDateDisplay={false}
       minDate={new Date()}
       disabledDates={disabledDates}
-      onChange={onChange}
     />
   );
 };
 
-export default Calendar;
+export default DatePicker;
--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import dynamic from "next/dynamic";
+import { IconType } from "react-icons";
+
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
-import { IconType } from "react-icons";
+
 import Avatar from "../Avatar";
 import ListingCategory from "./ListingCategory";
-import dynamic from "next/dynamic";
 
 const Map = dynamic(() => import("../Map"), {
   ssr: false,
@@ -37,17 +39,34 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
   locationValue,
 }) => {
   const { getByValue } = useCountries();
+
   const coordinates = getByValue(locationValue)?.latlng;
+
   return (
     <div className="col-span-4 flex flex-col gap-8">
       <div className="flex flex-col gap-2">
-        <div className="text-xl font-semibold flex flex-row items-center gap-2">
-          <div>Hosted by {user.name}</div>
+        <div
+          className="
+            text-xl 
+            font-semibold 
+            flex 
+            flex-row 
+            items-center
+            gap-2
+          "
+        >
+          <div>Hosted by {user?.name}</div>
           <Avatar src={user?.image} />
         </div>
         <div
           className="
-          flex flex-row items-center gap-4 font-light text-neutral-500"
+            flex 
+            flex-row 
+            items-center 
+            gap-4 
+            font-light
+            text-neutral-500
+          "
         >
           <div>{guestCount} guests</div>
           <div>{roomCount} rooms</div>
@@ -58,15 +77,21 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
       {category && (
         <ListingCategory
           icon={category.icon}
-          label={category.label}
-          description={category.description}
+          label={category?.label}
+          description={category?.description}
         />
       )}
       <hr />
-      <div className="text-lg font-light text-neutral-500">{description}</div>
+      <div
+        className="
+      text-lg font-light text-neutral-500"
+      >
+        {description}
+      </div>
       <hr />
-      <Map center={coordinates}></Map>
+      <Map center={coordinates} />
     </div>
   );
 };
+
 export default ListingInfo;
