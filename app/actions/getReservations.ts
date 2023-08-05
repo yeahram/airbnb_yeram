ce419b2078fc177407422c3bd826c162f0149a5d
yeahram <124855516+yeahram@users.noreply.github.com>
2023-08-05 14:12:48 +0900

    reservation오류 수정

--- app/actions/getReservations.ts
+++ app/actions/getReservations.ts
@@ -1,7 +1,7 @@
 import prisma from "@/app/libs/prismadb";
 
 interface IParams {
-  listingId?: String;
+  listingId?: string;
   userId?: string;
   authorId?: string;
 }
@@ -15,9 +15,11 @@ export default async function getReservations(params: IParams) {
     if (listingId) {
       query.listingId = listingId;
     }
+
     if (userId) {
       query.userId = userId;
     }
+
     if (authorId) {
       query.listing = { userId: authorId };
     }
@@ -27,7 +29,9 @@ export default async function getReservations(params: IParams) {
       include: {
         listing: true,
       },
-      orderBy: { createdAt: "desc" },
+      orderBy: {
+        createdAt: "desc",
+      },
     });
 
     const safeReservations = reservations.map((reservation) => ({
@@ -40,6 +44,7 @@ export default async function getReservations(params: IParams) {
         createdAt: reservation.listing.createdAt.toISOString(),
       },
     }));
+
     return safeReservations;
   } catch (error: any) {
     throw new Error(error);
--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -5,28 +5,33 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
+
   if (!currentUser) {
     return NextResponse.error();
   }
+
   const body = await request.json();
   const { listingId, startDate, endDate, totalPrice } = body;
 
   if (!listingId || !startDate || !endDate || !totalPrice) {
     return NextResponse.error();
   }
+
   const listingAndReservation = await prisma.listing.update({
-    where :{
-      id:listingId
+    where: {
+      id: listingId,
     },
-    data:{
-      reservations:{
-        create:{
-          userId:currentUser.id,
+    data: {
+      reservations: {
+        create: {
+          userId: currentUser.id,
           startDate,
           endDate,
-          totalPrice
-        }
-      }
-    }
+          totalPrice,
+        },
+      },
+    },
   });
+
+  return NextResponse.json(listingAndReservation);
 }
